19b4e865e86f93e6344fbb473a94b016eb10c5f7
Harsh-HK7 <harshkalal746@gmail.com>
2025-07-28 21:24:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(helmet());
 // CORS configuration
 
 app.use(cors({
-  origin: "https://cms-xi-flame.vercel.app/", // Vercel frontend URL
+  origin: "https://cms-xi-flame.vercel.app", // Vercel frontend URL
   credentials: true
 }));
 
